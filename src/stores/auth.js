b62e00b6a37f20733e4fc0476f431f3cd39a518a
Aryans <quispeloayza18@hotmail.com>
2024-03-30 12:17:12 -0500

    change

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -4,14 +4,18 @@ import { useFirebaseAuth } from 'vuefire'
 
 export const useAuthStore = defineStore('auth', () => {
   const auth = useFirebaseAuth()
+  const errorCode = {
+    'auth/user-not-found' : 'Usuario no encontrado',
+    'auth/wrong-password' : 'Password es incorrecto'
+  }
   const login = ({email,password}) => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         console.log(userCredential)
       })
-      .catch((error) => {
-        console.error(error.code)
-        console.error(error.message)
+      .catch(error => {
+        console.error(errorCode[error.code] || error.message)
+      
       })
   }
 
