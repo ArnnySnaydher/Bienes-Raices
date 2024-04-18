67a8ebcf39a87e8bcd93a8c521d9de9152952f98
Aryans <quispeloayza18@hotmail.com>
2024-04-18 17:23:07 -0500

    format precio

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import usePropiedades from '@/composables/usePropiedades'
 
-const {propiedadesCollection} =usePropiedades()
+const {propiedadesCollection,propertyPrice} =usePropiedades()
 
 </script>
 
@@ -9,16 +9,16 @@ const {propiedadesCollection} =usePropiedades()
   <h2 class="text-center text-h3 my-5 font-weight-bold">Admin Panel</h2>
   <v-btn color="blue" variant="flat" :to="{ name: 'nueva-propiedad' }">Nueva Propiedad</v-btn>
 
-  <v-card class="mx-auto mt-10" flat>
+  <v-card class="mx-auto mt-10" flat >
     <v-list>
       <v-list-item v-for="propiedad in propiedadesCollection" :key="propiedad.id">
         <template v-slot:prepend>
-          <v-list-item-media start="true">
+          <v-list-item-media :start="true">
             <img width="180" :src="propiedad.imagen">
           </v-list-item-media>
         </template>
         <v-list-item-title>{{ propiedad.titulo }}</v-list-item-title>
-        <v-list-item-subtitle>{{ propiedad.precio }}</v-list-item-subtitle>
+        <v-list-item-subtitle>{{ propertyPrice(propiedad.precio) }}</v-list-item-subtitle>
 
         <template v-slot:append>
           <v-btn color="info" flat class="mr-2">Editar</v-btn>
